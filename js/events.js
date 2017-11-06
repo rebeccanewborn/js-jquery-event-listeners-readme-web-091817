b9e706b83f365b0e9f7889cb52ce8236e3c2bab6
rebeccanewborn <rjn58@cornell.edu>
2017-11-06 18:28:11 -0500

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
 //define functions here
+function getIt() {
+	$("p").on("click", function() {
+		alert("clicked");
+	});
+}
 
-$(document).ready(function(){
+function frameIt() {
+	$("img").load(ev => {
+		ev.target.className = "tasty";
+	});
+}
 
-// call functions here
+function pressIt() {
+	$("#typing").on("keydown", function(key) {
+		if (key.which == 71) {
+			alert("g was pressed");
+		}
+	});
+}
 
+function submitIt() {
+	$("form").submit(function(ev) {
+		alert("Your form is going to be submitted now.");
+	});
+}
+
+$(document).ready(function() {
+	// call functions here
+	pressIt();
+	getIt();
+	frameIt();
+	submitIt();
 });
